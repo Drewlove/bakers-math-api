f0980d2cbeb1ac17b0bd060016e53195a0f03174
Drew Love <davidandrewlove84@gmail.com>
2021-04-26 09:58:19 -0400

    Add recipe table, routing and PostgreSQL logic with knex

---
+++ src/recipes/service.js
@@ -0,0 +1,33 @@
+const table = {
+  name: "recipe",
+  orderRow: "recipe_name",
+};
+const service = {
+  getAllRows(knex) {
+    return knex.select("*").from(table.name).orderBy(table.orderRow, "ASC");
+  },
+  getById(knex, row_id) {
+    return knex
+      .from(table.name)
+      .select("*")
+      .where(`${table.name}_id`, row_id)
+      .first();
+  },
+  insertRow(knex, newRow) {
+    return knex
+      .insert(newRow)
+      .into(table.name)
+      .returning("*")
+      .then((rows) => {
+        return rows[0];
+      });
+  },
+  updateRow(knex, row_id, newFields) {
+    return knex(table.name).where(`${table.name}_id`, row_id).update(newFields);
+  },
+  deleteRow(knex, row_id) {
+    return knex(table.name).where(`${table.name}_id`, row_id).delete();
+  },
+};
+
+module.exports = service;
